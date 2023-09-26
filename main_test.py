7d88b5219c45f4ec9e2dd8dcb6768d12d15979f2
marionegroponzi <marionegroponzi@gmail.com>
2023-09-26 10:53:18 +0200

    Initial commit

---
+++ main_test.py
@@ -0,0 +1,21 @@
+import unittest
+import os
+
+class LinksCheckerTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_example(self):
+        pass
+        # self.assertTrue("Privacy" in content)
+        # self.assertFalse(is_valid)
+        # self.assertIsNone(content)
+        # self.assertListEqual(valid_links, ["whereverwecometo/display/GRIDAD/",
+        #                             "https://www.polito.it"])
+
+
+if __name__ == '__main__':
+    unittest.main()
