21624f283ec609d3fcf75cec3243a71a2b8f37ef
Mario Negro Ponzi <marionegroponzi@gmail.com>
2023-09-29 21:34:46 +0200

    starting with files

--- usedproducts/__main__.py
+++ usedproducts/__main__.py
@@ -9,19 +9,17 @@ from lib.scanner import Scanner
 from lib.products import ProductsEncoder
 
 def crawl():
-    print('>>>>>>>>>> usedproducts crawling start <<<<<<<<<<')
+
     scanner = Scanner()
     num_pages = scanner.get_num_pages("https://www.usedproducts.nl/page/1/?s&post_type=product&vestiging=0")
     scanner.accept_cookies()
     # for page in range(1,num_pages+1):
-    for page in range(1,50):
+    for page in range(1,10):
         scanner.scan(f"https://www.usedproducts.nl/page/{page}/?s&post_type=product&vestiging=0")
     # print(ProductsEncoder().encode(scanner.products))
     scannedProducts = json.dumps(scanner.products, indent=4, cls=ProductsEncoder, ensure_ascii=False)
-    print(scannedProducts)
-
     scanner.close()
-    print('<<<<<<<<<< usedproducts crawling stop >>>>>>>>>>')
+    return scannedProducts
 
 def analyze():
     print('>>>>>>>>>> usedproducts analysis start <<<<<<<<<<')
@@ -33,7 +31,10 @@ def main():
     if not success:
         return
     config_env()
-    crawl()
+
+    products = crawl()
+    if args.save:
+        open(args.save.name, "w").write(products)
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -42,10 +43,10 @@ def parse_args():
         )
     parser.add_argument('--log', '-l', choices=['DEBUG','INFO','WARN','ERROR'],
                         help='loglevel: DEBUG, INFO, WARN, ERROR (default: ERROR)')
-    parser.add_argument('--load-file', '-f', type=argparse.FileType('r'), help='analyze data from a json file')
-    parser.add_argument('--save-file', '-s', type=argparse.FileType('w'), help='save data to a json file')
+    parser.add_argument('--load', '-f', type=argparse.FileType('r'), help='analyze data from a json file')
+    parser.add_argument('--save', '-s', type=argparse.FileType('w'), help='save data to a json file')
     args = parser.parse_args()
-    if args.log is not None:
+    if args.log:
         numeric_level = getattr(logging, args.log.upper())
         if not isinstance(numeric_level, int):
             raise ValueError('Invalid log level: %s' % args.loglevel)
