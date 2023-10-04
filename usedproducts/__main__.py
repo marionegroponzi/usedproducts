6b1cb822897a6dd78c3520f3fca74dcd2e4f98e2
Mario Negro Ponzi <marionegroponzi@gmail.com>
2023-10-04 12:51:53 +0200

    slightly changed logic

--- usedproducts/__main__.py
+++ usedproducts/__main__.py
@@ -45,14 +45,15 @@ def main():
         return
     config_env()
 
-    # if loading a file we assum one wants to add all derived data
-    # else we assume one want to store just the basics
-    if args.load:
+    # if load we assume one wants to refresh all derived data
+    # else we assume one wants to store them
+    if args.refresh:
         products_container = load(args.load.name)
-        products_container.fill_derived()
     else:
         products_container = crawl(args.max_pages)
         crawl_details(products_container.products)
+
+    products_container.fill_derived()
         
     if args.save:
         str_products = products_container.to_json()
@@ -67,7 +68,7 @@ def parse_args():
         )
     parser.add_argument('--log', '-l', choices=['DEBUG','INFO','WARN','ERROR'],
                         help='loglevel: DEBUG, INFO, WARN, ERROR (default: ERROR)')
-    parser.add_argument('--load', '-f', type=argparse.FileType('r'), help='analyze data from a json file')
+    parser.add_argument('--refresh', '-f', type=argparse.FileType('r'), help='load a json file instead of crawling and update the details for each product')
     parser.add_argument('--save', '-s', type=argparse.FileType('w'), help='save data to a json file')
     parser.add_argument('--max_pages', '-m', type=int, help='maximum number of pages to load', default=sys.maxsize)
     args = parser.parse_args()
