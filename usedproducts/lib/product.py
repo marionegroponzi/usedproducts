dc13d66df5e59e91fecb3577ef9907a880d26199
Mario Negro Ponzi <marionegroponzi@gmail.com>
2023-10-04 20:45:37 +0200

    many improvements with parsing

--- usedproducts/lib/product.py
+++ usedproducts/lib/product.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 class Product(object):
     description: str
     link: str
-    price: str
+    price_str: str
     details: str = ""
 
     ### derived members
@@ -18,8 +18,10 @@ class Product(object):
     model_name: str = ""
     battery_level: str = ""
     has_apple_garantie: bool = False
+    price_num: float = 0.0
 
     def fill_derived(self):
+        self.fill_price_num()
         self.fill_clean_details()
         self.fill_is_iphone()
         self.fill_is_ipad()
@@ -28,6 +30,13 @@ class Product(object):
         self.fill_battery_level()
         self.fill_has_apple_garantie()
 
+    def fill_price_num(self):
+        price = self.price_str
+        for c in ['€',' ','-','\.']:
+            price = re.sub(c, "", price)
+
+        price = re.sub(",", ".", price)
+        self.price_num = float(price)
 
     def fill_clean_details(self):
         d = re.sub("\n", " ", self.details)
@@ -60,24 +69,25 @@ class Product(object):
     def fill_model_name(self) -> Optional[str]:
         names = []
         models = []
-        for i in range (12, 16):
+        for i in range (11, 16):
             names.append(f"{i} pro max")
             names.append(f"{i} pro")
             names.append(f"{i} mini")
             names.append(f"{i} plus")
+            names.append(f"{i}")
 
-        names = names + ["xs max", "xs", "xr", "x", "x", "x", "6s", "6"]
+        names = names + ["xs max", "xs", "xr", "x", "x", "x", "6s", "6", "7 plus", "7"]
 
         names = names + ["se.*2022", "se.*2020"]
 
         for name in names:
-            model_name = name.replace("*.", " ")
+            model_name = name.replace(".*", " ")
             models.append({'match': 'iphone ' + name, 'name': 'iphone ' + model_name})
 
 
         names = ["pro 12.9", "pro.*2022", "pro.*2018", "pro.*2017", "pro 11", "2021", "2020", "2019", "2018", "mini 6", "mini 5", "mini", "air 2022", "air 10.9", "9e gen", "8e gen", "10th gen", "air 3" "air 2", "air 1", "air", ]
         for name in names:
-            model_name = name.replace("*.", " ")
+            model_name = name.replace(".*", " ")
             models.append({'match': 'ipad ' + name, 'name': 'ipad ' + model_name})
 
         for model in models:
