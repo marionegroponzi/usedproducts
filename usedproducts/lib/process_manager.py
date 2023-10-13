2583127143b1f1d3c82403e7c8fc5216d815b101
Mario Negro Ponzi <marionegroponzi@gmail.com>
2023-10-13 22:07:05 +0200

    it works again and better

--- usedproducts/lib/process_manager.py
+++ usedproducts/lib/process_manager.py
@@ -8,16 +8,17 @@ def dbsave(q_save: multiprocessing.Queue):
     db_manager = DBManager()
     db_manager.save_product(q_save)
 
-def crawldetails(queue_crawl_details: multiprocessing.Queue, queue_save: multiprocessing.Queue):
+def crawldetails(queue_crawl_details: multiprocessing.Queue, queue_save: multiprocessing.Queue, num_pages: int):
     crawler = Crawler()
     crawler.crawl_details(queue_crawl_details, queue_save)
 
-def crawlpage(queue_crawl_pages: multiprocessing.Queue, queue_crawl_details: multiprocessing.Queue, q_stop: multiprocessing.Queue):
+def crawlpage(queue_crawl_pages: multiprocessing.Queue, queue_crawl_details: multiprocessing.Queue, q_stop: multiprocessing.Queue, num_pages: int):
     crawler = Crawler()
-    crawler.crawl(queue_crawl_pages, queue_crawl_details, q_stop)
+    crawler.crawl(queue_crawl_pages, queue_crawl_details, q_stop, num_pages)
 
 class ProcessManager(object):
-    def __init__(self, q_stop):
+    num_pages: int
+    def __init__(self, q_stop, num_pages: int):
         ctx = multiprocessing.get_context('spawn')
         self.queue_crawl_pages = ctx.Queue()
         self.queue_crawl_details = ctx.Queue(maxsize=200)
@@ -25,8 +26,9 @@ class ProcessManager(object):
         self.save_process = Process(target=dbsave, args=(self.queue_save,))
         self.active_crawl_pages_processes = 12
         self.active_crawl_details_processes = 12
-        self.crawl_details_processes = [Process(target=crawldetails, args=(self.queue_crawl_details, self.queue_save,)) for _ in range(self.active_crawl_details_processes)]
-        self.crawl_pages_processes = [Process(target=crawlpage, args=(self.queue_crawl_pages, self.queue_crawl_details, q_stop, )) for _ in range(self.active_crawl_pages_processes)]
+        self.crawl_details_processes = [Process(target=crawldetails, args=(self.queue_crawl_details, self.queue_save, num_pages,)) for _ in range(self.active_crawl_details_processes)]
+        self.crawl_pages_processes = [Process(target=crawlpage, args=(self.queue_crawl_pages, self.queue_crawl_details, q_stop, num_pages,)) for _ in range(self.active_crawl_pages_processes)]
+        self.num_pages = num_pages
 
     def start(self):
         for process in self.crawl_pages_processes:
@@ -34,7 +36,7 @@ class ProcessManager(object):
         self.save_process.start()
         for process in self.crawl_details_processes:
             process.start()
-        # for i in range(self.num_pages): self.queue_crawl_pages.put(i)
+        for i in range(self.num_pages): self.queue_crawl_pages.put(i)
         
     def stop(self):
         for process in range(self.active_crawl_pages_processes):
@@ -47,6 +49,7 @@ class ProcessManager(object):
             process.join() 
         self.queue_save.put("finish")      
         self.save_process.join()
+
 
     def check_system_status(self):
         if psutil.virtual_memory().percent > 80:
