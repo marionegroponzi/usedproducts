d3fb86ed2ab85f688719ccc2ab352ec14acb8ed4
Mario Negro Ponzi <marionegroponzi@gmail.com>
2023-10-13 18:55:20 +0200

    create crawler and db manager classes

---
+++ usedproducts/lib/db_manager.py
@@ -0,0 +1,39 @@
+import multiprocessing
+import pymongo
+from lib.product import Product
+
+
+class DBManager(object):
+    def __init__(self):
+        self.coll = self.get_mongo()
+
+    def update_product_in_db(self, product: Product):
+        product.set_verified_date()  
+        self.coll.update_one({"link": product.link},{"$set": {"verified": product.verified }})
+
+    def already_stored(self, product: Product):
+        already_stored = self.coll.find_one({ "link": product.link })
+        return already_stored != None    
+    
+    def save_to_mongo(self, product: Product):
+        # pay attention that inserting the product modifies it. If you need to return it, copy the dict first
+        d = product.__dict__
+        self.coll.insert_one(d)
+
+    def get_mongo() -> pymongo.collection.Collection:
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        coll = client.usedproducts.products
+        return coll    
+    
+    def clear(self):
+        self.coll.delete_many({ })
+
+    def save_product(self, q_incoming: multiprocessing.Queue):
+        while(True):
+            incoming = q_incoming.get()
+            if type(incoming) is Product:
+                # print(f"Saving product {incoming.name}")
+                self.save_to_mongo(self, incoming, self.coll)
+            if type(incoming) is str:
+                print("Exiting save process")
+                return
--- usedproducts/lib/process_manager.py
+++ usedproducts/lib/process_manager.py
@@ -4,18 +4,22 @@ import time
 
 import psutil
 
+from lib.crawler import Crawler
+from lib.db_manager import DBManager
+
 class ProcessManager(object):
-    def __init__(self, save_fn, crawl_page_fn, crawl_details_fn, num_pages: int, q_stop):
+    # def __init__(self, save_fn, crawl_page_fn, crawl_details_fn, num_pages: int, q_stop):
+    def __init__(self, crawler:Crawler, db_manager:DBManager, q_stop):
         ctx = multiprocessing.get_context('spawn')
-        self.num_pages = num_pages
+        self.num_pages = crawler.num_pages
         self.queue_crawl_pages = ctx.Queue()
         self.queue_crawl_details = ctx.Queue(maxsize=200)
         self.queue_save = ctx.Queue()
-        self.save_process = Process(target=save_fn, args=(self.queue_save,))
+        self.save_process = Process(target=db_manager.save_fn, args=(self.queue_save,))
         self.active_crawl_pages_processes = 12
         self.active_crawl_details_processes = 12
-        self.crawl_details_processes = [Process(target=crawl_details_fn, args=(self.queue_crawl_details, self.queue_save,)) for _ in range(self.active_crawl_details_processes)]
-        self.crawl_pages_processes = [Process(target=crawl_page_fn, args=(self.queue_crawl_pages, self.queue_crawl_details, num_pages, q_stop, )) for _ in range(self.active_crawl_pages_processes)]
+        self.crawl_details_processes = [Process(target=crawler.crawl_details, args=(self.queue_crawl_details, self.queue_save,)) for _ in range(self.active_crawl_details_processes)]
+        self.crawl_pages_processes = [Process(target=crawler.crawl, args=(self.queue_crawl_pages, self.queue_crawl_details, crawler.num_pages, q_stop, )) for _ in range(self.active_crawl_pages_processes)]
 
     def start(self):
         for process in self.crawl_pages_processes:
